8bf3eabf74d9f44308b577e5a78708f1255edc2c
kontol66666 <akagaminojeki39@gmail.com>
2025-01-18 23:43:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const global = {
-    owner: [6142885267], // ganti jadi id mu
-    botToken: "7725260979:AAF9pls5ef2mKp0eZu-djXeFnnwC5XCRyL8", //isi make token bot mu
+    owner: [6521470375], // ganti jadi id mu
+    botToken: "7660240073:AAEsfvsbCnADL3a_GQLCOTqPm33dSbkQORw", //isi make token bot mu
 }
 
 module.exports = global;
